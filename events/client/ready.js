71cd2bcd1f3254aae2ef177bb8d5c3f2f0aa150c
M3TriX <frederickvalle14@gmail.com>
2022-05-23 10:30:24 -0600

    agregado motor de twitch para notificaciones

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,6 +7,7 @@ const { JSDOM } = jsdom;
 const got = require("got");
 const pool = require('../../db-connection.js');
 const {YTLive} = require("../../YTlive/YTlive");
+const { data } = require("cheerio/lib/api/attributes.js");
 /*var lastIdUrl;
 async function getLastId(urlx){
     let urs = "https://www.youtube.com/feeds/videos.xml?channel_id="+urlx;
@@ -26,9 +27,10 @@ module.exports = async(message,client) => {
     try {
         vtuberlist = await listVtubers();
         clientMessage = message; 
-        checkLives();
+        //checkLives();
+        checkTwitchLives();
         checkVtubers();
-        checklastVideoRSS();
+        //checklastVideoRSS();
         console.log("Init!!");
         /*setInterval(async function(){
         try {
@@ -226,6 +228,103 @@ async function checkLives(){
   }
 }
 
+async function getTwitchInfo(twitch){
+  uptime = await fetch(`https://decapi.me/twitch/uptime/${twitch}`);
+  avatar = "";
+  viewercount = "";
+  title = "";
+  game = "";
+  live = false;
+  uptime =  await uptime.text();
+  if(uptime !== `${twitch} is offline`){
+    avatar = await fetch(`https://decapi.me/twitch/avatar/${twitch}`);
+    viewercount = await fetch(`https://decapi.me/twitch/viewercount/${twitch}`);
+    title = await fetch(`https://decapi.me/twitch/title/${twitch}`);
+    game = await fetch(`https://decapi.me/twitch/game/${twitch}`);
+    avatar = await avatar.text();
+    viewercount = await viewercount.text();
+    title = await title.text();
+    game = await game.text();
+    live = true
+  }
+
+  return {uptime,avatar,viewercount,title,game,live}
+}
+
+function toseconds(timestr){
+  valor = timestr.split(',')
+  seconds = 0
+  for(let time of valor){
+    let val = time.split(' ')
+    if(val[val.length-1].match(/hour/)) seconds+=(val[val.length-2]*60*60)
+    if(val[val.length-1].match(/minute/)) seconds+=(val[val.length-2]*60)
+    if(val[val.length-1].match(/second/)) seconds+=(val[val.length-2]*1)
+    console.log(val)
+  }
+  return seconds
+}
+
+async function sendDTMessage(live,estado,vTuber,timestamp){
+  let img =  live.avatar;
+  let view ="???",timestr="",Status ="";
+  if(estado===1){
+    Status = `En Vivo`;
+    view = live.viewercount;
+    starmili = (timestamp.getTime())-(toseconds(live.uptime)*1000);
+    timestr = `Empezo <t:${(starmili/1000).toFixed(0)}:R>`;
+  }
+  let embed = new Discord.MessageEmbed()
+      .setTitle(`${live.title}`)
+      .setImage(img)
+      .setDescription(vTuber.twitch)
+      .setThumbnail(vTuber.logo)
+      .setColor(vTuber.value_color)
+      .addFields(
+      { name: 'Status', value: `${Status}`, inline: true },
+      { name: 'Time', value: `${timestr}`, inline: true },
+      { name: 'Viewers', value: `${view}`, inline: true },
+      { name: 'Game', value: `${live.game}`, inline: true }
+      )
+      .setTimestamp()
+      .setURL("https://www.twitch.tv/"+vTuber.twitch)
+  clientMessage.channels.cache.get(vTuber.canal_alerta_key).send(embed);
+}
+
+async function checkTwitchLives(){
+  try{
+    setInterval(async function(){
+      for (let vtuber of vtuberlist) {
+        if(vtuber.twitch!=='-'){
+          try {
+            timestamp = new Date()
+            let data = await getTwitchInfo(vtuber.twitch)
+            console.log(data)
+            console.log(`TLIVE: ${vtuber.twitch}`)
+            let estadodb = await stateTLivedb(vtuber.id_vtuber)
+            if(data.live != estadodb[0].tlive){
+              if(data.live){
+                sendDTMessage(data, 1, vtuber,timestamp);
+                updateTLiveStatedb(vtuber.id_vtuber,1)
+              }else{
+                updateTLiveStatedb(vtuber.id_vtuber,0)
+              }
+              console.log(`TLIVE: ${vtuber.twitch}`)
+            }
+          } catch (error) {
+            console.error(`TLIVE: ${error}`);
+            console.log(`TLIVE: ${vtuber.twitch}`)
+          }
+          await sleep(300);
+        }else{
+          console.log(`TLIVE: ${vtuber.nombre}`)
+        }
+      }
+    }, 20000); 
+  } catch(err) {
+    console.log(err);
+  }
+}
+
 async function sleep(ms) {
     return new Promise((resolve) => {
       setTimeout(resolve, ms);
@@ -342,7 +441,7 @@ async function sleep(ms) {
         .setTimestamp()
         .setURL("https://www.youtube.com/watch?v="+live.liveId)
     clientMessage.channels.cache.get(vTuber.canal_alerta_key).send(embed);
-}
+  }
     function convertMS(ms) {
         var d, h, m, s;
         s = Math.floor(ms / 1000);
@@ -354,7 +453,7 @@ async function sleep(ms) {
         h = h % 24;
         h += d * 24;
         return ((h!==0)?(h<10)?'0'+h+':':h+':':'')+((m<10)?'0'+m:m)+':'+((s<10)?'0'+s:s);
-}
+  }
 
   async function setLivedb(vTuber,liveId,estado){
     return await pool.query(`INSERT INTO Pjt3W34Qzv.video(id_videos,id_vtuber,id_video,estado) VALUES(NULL,'${vTuber.id_vtuber}','${liveId}',${estado})`);
@@ -364,32 +463,40 @@ async function sleep(ms) {
     return await pool.query(`UPDATE Pjt3W34Qzv.video SET estado = ${estado} WHERE id_video = '${liveId}';`);
   }
   
+  async function updateTLiveStatedb(vtuberId,estado){
+    return await pool.query(`UPDATE Pjt3W34Qzv.vtuberlist SET tlive = '${estado}' WHERE id_vtuber = ${vtuberId};`);
+  }
+
   async function existLivedb(live){
     list = await pool.query(`SELECT * FROM Pjt3W34Qzv.video WHERE id_video='${live}'`);
     return (list.length!=0)
-}
+  }
 
-function loadVTObjects(vtuberlist){
-    vtuberlist.forEach(element => {
-        yTlives.push(new YTLive({channelId: element.channelid}))
-        console.log(`LOADED: ${element.nombre} ${element.channelid}`)
-    });
-}
-function updateVTlist(list){
-    list.forEach(element => {
-        vtuberlist.push(new YTLive({channelId: element.channelid}))
-        console.log(`ADDED: ${element.nombre} ${element.channelid}`)
-    });
-}
+  async function stateTLivedb(vtuberId){
+    return pool.query(`SELECT tlive FROM Pjt3W34Qzv.vtuberlist WHERE id_vtuber = '${vtuberId}'`);
+  }
 
-async function listVtubers(){
-    return await pool.query(`select v.id_vtuber,v.nombre,v.channelid,v.activo,a.canal_alerta_key,
-        c.value_color,e.logo
-        from vtuberlist v 
-        inner join canal_alerta a
-        on v.id_canal_alerta = a.id_canal_alerta
-        inner join empresa e
-        on v.id_empresa = e.id_empresa
-        inner join colores c
-        on v.id_color=c.id_color`);
-}
+  function loadVTObjects(vtuberlist){
+      vtuberlist.forEach(element => {
+          yTlives.push(new YTLive({channelId: element.channelid}))
+          console.log(`LOADED: ${element.nombre} ${element.channelid}`)
+      });
+  }
+  function updateVTlist(list){
+      list.forEach(element => {
+          vtuberlist.push(new YTLive({channelId: element.channelid}))
+          console.log(`ADDED: ${element.nombre} ${element.channelid}`)
+      });
+  }
+
+  async function listVtubers(){
+      return await pool.query(`select v.id_vtuber,v.nombre,v.channelid,v.twitch,v.activo,a.canal_alerta_key,
+          c.value_color,e.logo
+          from vtuberlist v 
+          inner join canal_alerta a
+          on v.id_canal_alerta = a.id_canal_alerta
+          inner join empresa e
+          on v.id_empresa = e.id_empresa
+          inner join colores c
+          on v.id_color=c.id_color`);
+  }
